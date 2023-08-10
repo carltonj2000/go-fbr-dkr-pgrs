f5aadf749f469ea52540807571d052ed7231961b
Carlton Joseph <carlton.joseph@gmail.com>
2023-08-09 17:43:34 -0700

    add live reload - really this time

--- cmd/main.go
+++ cmd/main.go
@@ -6,7 +6,7 @@ func main() {
     app := fiber.New()
 
     app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, Carlton!")
+        return c.SendString("Hello, Carlton Two!")
     })
 
     app.Listen(":3000")
