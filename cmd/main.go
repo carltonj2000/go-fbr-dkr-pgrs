b940844faedf8027c2c751dc041b8f9cde27a060
Carlton Joseph <carlton.joseph@gmail.com>
2023-08-10 19:10:51 -0700

    improved ui

--- cmd/main.go
+++ cmd/main.go
@@ -12,5 +12,6 @@ func main() {
 	engine := html.New("./views", ".html")
 	app := fiber.New(fiber.Config{Views: engine, ViewsLayout: "layouts/main"})
 	setupRoutes(app)
+	app.Static("/", "./public")
 	app.Listen(":3000")
 }
